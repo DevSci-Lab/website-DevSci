253f242ab0a4fd4364bfa3bb619fcd3c072e57b2
David Ibarra <david.ibarral0798@gmail.com>
2026-01-07 20:38:10 +0100

    configured the creation of CNAME file for deployment

--- freeze.py
+++ freeze.py
@@ -1,4 +1,9 @@
 from app import app, freezer
+import os
 
 if __name__ == '__main__':
     freezer.freeze()
+
+    cname_path = os.path.join('docs', 'CNAME')
+    with open(cname_path, 'w') as f:
+        f.write('devscilab.com')
